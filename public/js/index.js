3e6227a44d129b4e8f3ee71af1901c2b6d403f86
Naitzz12 <naitzz76@gmail.com>
2024-09-28 13:14:12 +0700

    Fix error get API

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,52 @@
-const dataKu2 =
-  "https://newsapi.org/v2/top-headlines?country=id&apiKey=bd7f9541754b41229ec2a0e55621c35d";
-const dataKu = "https://jakpost.vercel.app/api/category/business/tech/page/2";
-const dataKu3 =
-  "https://newsdata.io/api/1/news?apikey=pub_4996508e8b612e46dbf5f69824c9951eff174&q=news&country=id&language=id&category=education,sports,technology ";
+// const dataKu2 =
+//   "https://newsapi.org/v2/top-headlines?country=id&apiKey=bd7f9541754b41229ec2a0e55621c35d";
+// const dataKu = "https://jakpost.vercel.app/api/category/business/tech/page/2";
+// const dataKu3 =
+//   "https://newsdata.io/api/1/news?apikey=pub_4996508e8b612e46dbf5f69824c9951eff174&q=news&country=id&language=id&category=education,sports,technology ";
 const dataKu4 = "https://api-berita-indonesia.vercel.app/antara/terbaru";
-const dataKu5 = "http://youth-dt.gl.at.ply.gg:42473/antara/terbaru";
+// const dataKu5 = "http://youth-dt.gl.at.ply.gg:42473/antara/terbaru";
 const dataMem = "../database/saran.json";
+
+function scrollingNavbar() {
+  const nav = document.querySelector("nav");
+  const scrollYtop = window.scrollY;
+  if (scrollYtop > 250) {
+    nav.classList.add("top");
+  } else {
+    nav.classList.remove("top");
+  }
+}
+
+window.addEventListener("scroll", scrollingNavbar);
 
 async function fetchApi() {
   const api = await fetch(dataKu4);
-  const api2 = await fetch(dataMem);
-  const data2 = await api2.json();
   const data = await api.json();
-  console.log(data.data.posts);
-  console.log(data2);
+  // console.log(data.data.posts);
+  const col = document.querySelector(".blog-list");
+  const getData = data.data.posts;
+  for (let i = 0; i < getData.length; ++i) {
+    console.log(getData[i]);
+    const colloum = document.createElement("div");
+    colloum.className = "mydata";
+    col.appendChild(colloum);
 
-  let dataL = `
-  <div class="mydata">
-  <img src="${data.data.posts[0].thumbnail}" />
-  <h3>${data.data.posts[0].title}</h3>
-  <p>${data.data.posts[0].description}</p>
-  <a href="${data.data.posts[0].link}">Baca Berita </a>
-  </div>
-  <div class="mydata">
-  <img src="${data.data.posts[1].thumbnail}" />
-  <h3>${data.data.posts[1].title}</h3>
-  <p>${data.data.posts[1].description}</p>
-  <a href="${data.data.posts[1].link}">Baca Berita </a>
-  </div>
-  <div class="mydata">
-  <img src="${data.data.posts[2].thumbnail}" />
-  <h3>${data.data.posts[2].title}</h3>
-  <p>${data.data.posts[2].description}</p>
-  <a href="${data.data.posts[2].link}">Baca Berita </a>
-  </div>
-  <div class="mydata">
-  <img src="${data.data.posts[3].thumbnail}" />
-  <h3>${data.data.posts[3].title}</h3>
-  <p>${data.data.posts[3].description}</p>
-  <a href="${data.data.posts[3].link}">Baca Berita </a>
-  </div>
-  `;
+    const img = document.createElement("img");
+    img.src = getData[i].thumbnail;
+    colloum.appendChild(img);
+    const h3 = document.createElement("h3");
+    h3.textContent = getData[i].title;
+    colloum.appendChild(h3);
+    const p = document.createElement("p");
+    p.textContent = getData[i].description;
+    colloum.appendChild(p);
+    const a = document.createElement("a");
+    a.textContent = "Baca Berita";
+    a.href = getData[i].link;
+    colloum.appendChild(a);
 
-  document.querySelector(".blog-list").innerHTML = dataL;
+    col.appendChild(colloum);
+  }
 }
 
 fetchApi();
-
-// async function apiShow() {
-//   const api = await fetch(dataKu4);
-//   const data = await api.json();
-//   console.log(data);
-// }
-// apiShow();
