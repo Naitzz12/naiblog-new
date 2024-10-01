a2054ae536650e1172970de260901e01e7f0314b
Naitzz12 <naitzz76@gmail.com>
2024-10-01 22:56:43 +0700

    Update UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,13 @@
+// const dataKu5 = "http://youth-dt.gl.at.ply.gg:42473/antara/terbaru";
 // const dataKu2 =
 //   "https://newsapi.org/v2/top-headlines?country=id&apiKey=bd7f9541754b41229ec2a0e55621c35d";
 // const dataKu = "https://jakpost.vercel.app/api/category/business/tech/page/2";
 // const dataKu3 =
 //   "https://newsdata.io/api/1/news?apikey=pub_4996508e8b612e46dbf5f69824c9951eff174&q=news&country=id&language=id&category=education,sports,technology ";
+
+// const fs = require("fs");
+
 const dataKu4 = "https://api-berita-indonesia.vercel.app/antara/terbaru";
-// const dataKu5 = "http://youth-dt.gl.at.ply.gg:42473/antara/terbaru";
 const dataMem = "../database/saran.json";
 
 function scrollingNavbar() {
@@ -26,7 +29,7 @@ async function fetchApi() {
   const col = document.querySelector(".blog-list");
   const getData = data.data.posts;
   for (let i = 0; i < getData.length; ++i) {
-    console.log(getData[i]);
+    // console.log(getData[i]);
     const colloum = document.createElement("div");
     colloum.className = "mydata";
     col.appendChild(colloum);
